72254df87033c79b1ebca0ef5bb3bd86d71a1017
Finn Drabsch <fsd16@uclive.ac.nz>
2023-08-19 19:35:18 +1200

    Add backup functionality for original playlists

--- src/app/spotifyautoshuffle.py
+++ src/app/spotifyautoshuffle.py
@@ -1,38 +1,61 @@
-#https://developer.spotify.com/dashboard/8514735c3caf439490cf06059cd0c269/settings
+"""
+Spotify Playlist Shuffler and Backup Script
+
+This script retrieves playlists for a user from their Spotify account, shuffles the tracks within each playlist, overwrites the shuffled playlists back to Spotify, and creates backups of the original playlists in JSON format.
+
+Author: Finn Drabsch
+Date: 19/08/2023
+
+Usage:
+1. Ensure you have registered your application on the Spotify Developer Dashboard to obtain client credentials.
+2. Create a .env file in the script directory containing:
+
+    SPOTIPY_CLIENT_ID = <your_client_id>
+    SPOTIPY_CLIENT_SECRET = <your_client_secret>
+    SPOTIPY_REDIRECT_URI = <your_redirect_uri>
+
+3. Run the script to shuffle playlists and create backups.
+
+Note:
+- This script requires the 'spotipy' and 'python-dotenv' packages, which can be installed using 'pip install spotipy python-dotenv'.
+
+"""
 
 import logging
 import os
-import random
-import json
+from datetime import datetime
+from json import dumps
 from pathlib import Path
+from random import shuffle
+from zipfile import ZIP_LZMA, ZipFile
 
 from dotenv import load_dotenv
 from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth, CacheFileHandler
+from spotipy.oauth2 import CacheFileHandler, SpotifyOAuth
 
-def get_playlist_tracks(playlist_id, *args, **kwargs):
-    results = sp.playlist_tracks(playlist_id, *args, **kwargs)
-    tracks = results['items']
-    while results['next']:
-        results = sp.next(results)
-        tracks.extend(results['items'])
-    return tracks
+# Generate the current timestamp
+_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
+# Get the script directory
 _dir = Path(__file__).resolve().parent
 
-log_dir = _dir.parent.parent.joinpath('logs', 'schedule.log')
+# Initilize logging and backup directories
+log_dir = _dir.parent.parent.joinpath('logs')
+bkup_dir = _dir.parent.parent.joinpath('backups')
 
-# Create a custom logger
+log_filename = log_dir.joinpath('autoshuffle.log')
+
+# Create custom loggers
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 # Create handlers
 c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler(log_dir)
+f_handler = logging.FileHandler(log_filename)
 # c_handler.setLevel(logging.INFO)
 # f_handler.setLevel(logging.INFO)
 
-# Create formatters and add it to handlers
+# Create formatters and add to handlers
 c_format = logging.Formatter('%(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 c_handler.setFormatter(c_format)
@@ -42,9 +65,10 @@ f_handler.setFormatter(f_format)
 log.addHandler(c_handler)
 log.addHandler(f_handler)
 
+# Load environment variables from the local .env
 load_dotenv()
 
-# Set your Spotify API credentials
+# Initilize Spotify API credentials
 client_id = os.environ.get('SPOTIPY_CLIENT_ID')
 client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
 redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI')  # Redirect URI you specified in the Spotify Developer Dashboard
@@ -52,34 +76,44 @@ redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI')  # Redirect URI you specif
 # Define the required scopes
 scopes = ['playlist-modify-public', 'playlist-modify-private']
 
-# Initialize the Spotify OAuth client
+# Initialize the Spotify API client
 log.info("Initializng the Spotify API client")
 sp = Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri, scope=scopes, cache_handler=CacheFileHandler(_dir.joinpath('.cache'))))
 
+# Initialize the user id
 user_id = sp.me()['id']
 
-# playlists to shuffle
+# Get all user playlists
 log.info("Requesting user playlists")
 playlists = sp.current_user_playlists()['items']
 
+# Initilize list to store playlist backups
+backup = list()
+
+# Store original playlists for backup, shuffle each playlist, and overwrite shuffled playlists to spotify
 log.info("Shuffling user playlists")
 for playlist in playlists:
+    # Only shuffle playlist owned by the user
     if playlist['owner']['id'] == user_id:
 
-        # Retrieve playlist tracks
+        # Retrieve playlist tracks (have to do this iteratively due to limit of 100 items)
         results = sp.playlist_items(playlist['id'])
         tracks = results['items']
         while results['next']:
             results = sp.next(results)
             tracks.extend(results['items'])
+        
+        # Store tracks for back up
+        playlist['tracks'].update({'items': tracks})
+        backup.append(playlist)
 
         # Extract the track URIs
         track_uris = [track['track']['uri'] for track in tracks]
 
         # Shuffle the track URIs
-        random.shuffle(track_uris)
+        shuffle(track_uris)
 
-        # Replace the playlist items with shuffled items
+        # Overwrite the original Spotify playlist with the shuffled list (have to do this iteratively due to limit of 100 items)
         sp.playlist_replace_items(playlist['id'], [])
         batch_size = 100
         track_batches = [track_uris[i:i+batch_size] for i in range(0, len(track_uris), batch_size)]
@@ -89,3 +123,12 @@ for playlist in playlists:
         log.info(f'{playlist["name"]} shuffled successfully!')
 
 log.info("Shuffling complete!")
+
+# Back up the original playlists
+log.info("Backing up the original playlists")
+bkup_filename_json = Path(f'playlists_{_timestamp}.json')
+bkup_filename_zip = bkup_dir.joinpath(bkup_filename_json.with_suffix('.zip'))
+with ZipFile(bkup_filename_zip, 'w', ZIP_LZMA) as zipf:
+        zipf.writestr(str(bkup_filename_json), dumps(backup, indent=4))
+
+log.info("Back up complete!")
